872a29167996eabc775f0589f3d6892e79ecf8ed
FonadFeco367 <womaferi80@gmail.com>
2026-09-21 10:14:42 +0200

    harmadik szinkron

--- masodik.py
+++ masodik.py
@@ -18,3 +18,16 @@ hallgato = {"nev": 'Jolán', "kor": 19}
 print(hallgato["nev"])
 print(halmaz)
 print('Szia', felhasznalo_neve, "!", jegyek)
+
+print('Jó', 'reggelt', 'DUE!',end='\n\n', sep='-')
+print('Több soros\n'
+      'kiírás\n'
+      '!!!!')
+
+print(f'Szia {felhasznalo_neve}! \n{jegyek}')
+print(f'Kora: {felhasznalo_kora: .2f}')
+
+print(felhasznalo_neve.rjust(30, '.'))
+print(felhasznalo_neve.ljust(30,'.'))
+print(felhasznalo_neve.center(30, '.'))
+print(str(felhasznalo_kora).center(30))
